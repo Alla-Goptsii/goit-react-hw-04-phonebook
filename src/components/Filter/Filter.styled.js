4694190c99287ed082324ca1cdeaeada5d217a4d
Alla-Goptsii <goptsy.a@gmail.com>
2022-11-10 12:16:39 +0200

    delete

--- src/components/Filter/Filter.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Label = styled.label`
-  border-radius: 4px;
-  width: 100px;
-  height: 20px;
-  align-items: center;
-  margin-left: 30px;
-`;
